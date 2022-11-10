354ae1998dac83ded8160bb941fe620ca695d32d
larrydaguilera <larrydaguilera@gmail.com>
2022-11-10 20:03:58 -0300

    Springboot project initialized

---
+++ Backend/Babybook/src/main/java/com/ncs503/Babybook/BabybookApplication.java
@@ -0,0 +1,13 @@
+package com.ncs503.Babybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabybookApplication.class, args);
+	}
+
+}
