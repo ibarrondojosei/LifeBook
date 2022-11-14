52bd8409d46d210f3c061d9b692f3d8b16374899
ibarrondojosei <94654582+ibarrondojosei@users.noreply.github.com>
2022-11-14 19:06:20 -0300

    Added SendGrid Config and Dependencies in pom for Swagger, SendGrid, AWS (#4)

--- Backend/Babybook/src/main/java/com/ncs503/Babybook/BabybookApplication.java
+++ Backend/Babybook/src/main/java/com/ncs503/Babybook/BabybookApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+
 //@SpringBootApplication
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
 public class BabybookApplication {
 
 	public static void main(String[] args) {
