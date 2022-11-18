29d212bd302ac5cef86c55c23890bb93dca4f210
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-11-18 15:01:56 -0300

    added ClinicalHistory main page

---
+++ Frontend/my-app/src/view/ClinicalHistory/ClinicalHistory.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+export default function ClinicalHistory() {
+  return (
+	<div className="absolute top-10 left-20">
+		<h1 className=' text-black-800 text-5xl font-bold'>
+		Historia Clínica
+		</h1>
+		<div className='absolute space-y-10 top-20 left-10'>
+		<h3 className='text-black-800 text-2xl'>2021</h3>
+		<h3 className='text-black-800 text-2xl'>2020</h3>
+		
+		<h3 className='text-black-800 text-2xl'>2019</h3>
+		
+		<h3 className='text-black-800 text-2xl'>2018</h3>
+		
+		<h3 className='text-black-800 text-2xl'>2017</h3>
+		
+		<h3 className='text-black-800 text-2xl'>2016</h3>
+		
+		<h3 className='text-black-800 text-2xl'>2015</h3>
+		</div>
+	</div>
+  )
+}
