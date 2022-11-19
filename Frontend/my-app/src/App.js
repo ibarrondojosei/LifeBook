d9255f5fa36e3132dae28ccefc53dbb2af2a0dac
Leandro Martini <leandromartini86@gmail.com>
2022-11-19 20:25:10 -0300

    arreglos login

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,10 +1,12 @@
 
 import { LoginView } from './View/Login/LoginView';
+import { RegisterView } from './View/Register/RegisterView';
 
 function App() {
   return (
     <div className="App">
       <LoginView/>
+      {/* <RegisterView/> */}
   
     </div>
   );
--- Frontend/my-app/src/View/Register/RegisterView.js
+++ Frontend/my-app/src/View/Register/RegisterView.js
@@ -1,27 +1,14 @@
 import React from "react";
-import "./LoginView.css";
+import "./RegisterView.css"
 
-export const LoginView = () => {
+export const RegisterView = () => {
   return (
-    <div className="login">
-      
-      <div className="  w-full max-w-xs float-right m-36  ">
-        
+    <div className="register">
+      <div className="  w-full max-w-xs float-right m-20  ">
         <form className=" bg-white shadow-md rounded-lg px-12 pt-6 pb-8 w-96  ">
           <h1 className="text-gray-800 text-center font-bold p-4 pb-8">
-            Inicio de sesion
+            Registrarme
           </h1>
-          <div className="mb-8">
-            <div class="flex justify-center items-center">
-              <label className="mr-">¿Eres nuevo usuario?</label>
-              <a
-              
-                className=" text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"
-              >
-                Crear una cuenta
-              </a>
-            </div>
-          </div>
 
           <div className="mb-4">
             <label
@@ -51,32 +38,45 @@ export const LoginView = () => {
               placeholder="password"
             />
           </div>
-          <div className="flex items-center justify-between">
-					<div className="flex items-center">
-						<input id="recuerdame" name="recuerdame" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
-						<label for="remember_me" class="ml-2 block text-sm text-gray-900">
-							Recuerdame
-						</label>
-					</div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="password"
+            />
+          </div>
 
-					<div className="text-sm">
-						<a  className="font-medium text-indigo-600 hover:text-indigo-500">
-							Olvido su contraseña?
-						</a>
-					</div>
-				</div>
           <div>
-          
             <button
               className="  block bg-teal-400 hover:bg-teal-600 font-bold text-black uppercase text-lg mx-auto p-2 rounded mt-4"
               type="button"
             >
-              Ingresar
+              Registrar
             </button>
           </div>
         </form>
       </div>
-      <img className="ml-16 pt-32" src="https://res.cloudinary.com/dytpump6i/image/upload/v1668705729/LifeBook2_2_1_kdgf1m.png" alt="" />
     </div>
   );
 };
