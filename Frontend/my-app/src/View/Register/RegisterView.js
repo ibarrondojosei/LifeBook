82acc2fe2eec57f795048a81ff474a6be6b1e8e3
Leandro Martini <leandromartini86@gmail.com>
2022-11-21 18:44:35 -0300

    login y register - fontawesone

--- Frontend/my-app/src/View/Register/RegisterView.js
+++ Frontend/my-app/src/View/Register/RegisterView.js
@@ -4,24 +4,38 @@ import "./RegisterView.css"
 export const RegisterView = () => {
   return (
     <div className="register">
-      <div className="  w-full max-w-xs float-right m-20  ">
+      <div className="  w-full max-w-xs float-right mr-36 mt-12   ">
         <form className=" bg-white shadow-md rounded-lg px-12 pt-6 pb-8 w-96  ">
           <h1 className="text-gray-800 text-center font-bold p-4 pb-8">
-            Registrarme
+           Registrar
           </h1>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Nombre
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Nombre"
+            />
+          </div>
 
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
               for="username"
             >
-              Username
+              Apellido
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
-              placeholder="Username"
+              placeholder="Apellido"
             />
           </div>
           <div className="mb-6">
@@ -29,13 +43,13 @@ export const RegisterView = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               for="password"
             >
-              Password
+              Nombre de usuario
             </label>
             <input
               className="shadow appearance-none border b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder="password"
+              placeholder="Nombre de usuario"
             />
           </div>
           <div className="mb-6">
@@ -43,13 +57,13 @@ export const RegisterView = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               for="password"
             >
-              Password
+              Mail
             </label>
             <input
               className="shadow appearance-none border b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder="password"
+              placeholder="Mail"
             />
           </div>
           <div className="mb-6">
@@ -57,13 +71,13 @@ export const RegisterView = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               for="password"
             >
-              Password
+              Contraseña
             </label>
             <input
               className="shadow appearance-none border b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder="password"
+              placeholder="Contraseña"
             />
           </div>
 
