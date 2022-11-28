ff5fc68a2e3576a3a614687fdbbc66e99f13ab99
Leonardo <terlizzileonardo@gmail.com>
2022-11-28 10:58:10 -0300

    added token validation to user entity controller endpoints

--- Backend/Babybook/src/main/java/com/ncs503/Babybook/controller/UserEntityController.java
+++ Backend/Babybook/src/main/java/com/ncs503/Babybook/controller/UserEntityController.java
@@ -17,14 +17,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Leonardo Terlizzi
@@ -49,17 +42,19 @@ public class UserEntityController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    @ApiOperation(value = "Get an user", notes = "Endpoint that return an user by id")
+    @GetMapping("/get")
+    @ApiOperation(value = "Get an user", notes = "Endpoint that return an user by the info inside the token")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "User information"),
             @ApiResponse(code = 403, message = "Forbidden action")
     })
-    public ResponseEntity<UserResponse> getUser(@PathVariable("id") Long id) throws UserNotFoundException {
-        return new ResponseEntity<>(userServ.getUser(id), HttpStatus.OK);
+    public ResponseEntity<UserResponse> getUser(@RequestHeader(name="Authorization") String token
+                                                ) throws UserNotFoundException, InvalidUserException {
+        return new ResponseEntity<>(userServ.getUser(token), HttpStatus.OK);
     }
 
-    //new user in register controller
+
+
 
     @DeleteMapping("/delete")
     @ApiOperation(value = "Delete an User", notes = "Endpoint that allows an user to delete himself/herself")
@@ -67,45 +62,54 @@ public class UserEntityController {
             @ApiResponse(code = 201, message = "User deleted"),
             @ApiResponse(code = 403, message = "Forbbiden action")
     })
-    public ResponseEntity<Void> deleteUser(@RequestParam Long id) throws UserNotFoundException {
-        userServ.deleteUser(id);
+    public ResponseEntity<Void> deleteUser(@RequestParam Long id,
+                                           @RequestHeader(name="Authorization") String token)
+            throws UserNotFoundException, InvalidUserException {
+        userServ.deleteUser(id, token);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/su/delete")
-//    @ApiOperation(value = "Delete an User(admin)", notes ="Endpoint that allows an admin to delete an user")
-//    @ApiResponses(value = {
-//            @ApiResponse( code = 201, message = "User deleted"),
-//            @ApiResponse( code = 403, message = "Forbbiden action")
-//    })
-    public ResponseEntity<Void> adminDeleteUser(@RequestParam Long id) throws UserNotFoundException {
-        userServ.deleteUser(id);
+    @ApiOperation(value = "Delete an User(admin)", notes ="Endpoint that allows an admin to delete an user")
+    @ApiResponses(value = {
+            @ApiResponse( code = 201, message = "User deleted"),
+            @ApiResponse( code = 403, message = "Forbbiden action")
+    })
+    public ResponseEntity<Void> adminDeleteUser(@RequestHeader(name="Authorizaation") String token,
+                                                @RequestParam Long id)
+            throws UserNotFoundException, InvalidUserException {
+        userServ.deleteUser(id, token);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 
     @PatchMapping("/update")
-//    @ApiOperation(value = "Update an User", notes ="Endpoint that allows to update user's information")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 201, message = "User updated"),
-//            @ApiResponse(code = 403, message = "Forbbiden action")
-//    })
-    public ResponseEntity<UserResponse> editUser(@RequestBody UpdateUserRequest userReq, @RequestParam Long id) throws InvalidUserException, UserNotFoundException {
-        userServ.updateUser(userReq, id);
-       // return ResponseEntity.status(HttpStatus.OK).build();
-        return new ResponseEntity<>(userServ.getUser(id), HttpStatus.OK);
+    @ApiOperation(value = "Update an User", notes ="Endpoint that allows to update user's information")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User updated"),
+            @ApiResponse(code = 403, message = "Forbbiden action")
+    })
+    public ResponseEntity<UserResponse> editUser(@RequestHeader(name="Authorization") String token,
+                                                    @RequestBody UpdateUserRequest userReq,
+                                                    @RequestParam Long id)
+                                        throws InvalidUserException, UserNotFoundException {
+        userServ.updateUser(userReq, id, token);
+        return new ResponseEntity<>(userServ.getUser(token), HttpStatus.OK);
 
     }
 
     @PatchMapping("/su/update")
-//    @ApiOperation(value = "Update an User(admin)", notes ="Endpoint that allows an admin to update an user's information")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 201, message = "User updated"),
-//            @ApiResponse(code = 403, message = "Forbbiden action")
-//    })
-    public ResponseEntity<UserResponse> adminEditUser(@RequestBody UpdateUserRequest userReq, @RequestParam Long id) throws InvalidUserException, UserNotFoundException {
-        userServ.updateUser(userReq, id);
-        return new ResponseEntity<>(userServ.getUser(id), HttpStatus.OK);
+    @ApiOperation(value = "Update an User(admin)", notes ="Endpoint that allows an admin to update an user's information")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User updated"),
+            @ApiResponse(code = 403, message = "Forbbiden action")
+    })
+    public ResponseEntity<UserResponse> adminEditUser(@RequestBody UpdateUserRequest userReq,
+                                                      @RequestHeader(name="Authorization") String token,
+                                                      @RequestParam Long id)
+                                        throws InvalidUserException, UserNotFoundException {
+        userServ.updateUser(userReq, id, token);
+        return new ResponseEntity<>(userServ.getUser(token), HttpStatus.OK);
 
     }
 
