43afab0d3448ece5f675ee2970e60394cd3f75a4
Leandro Martini <leandromartini86@gmail.com>
2022-12-01 09:54:39 -0300

    detalles

--- Frontend/my-app/src/View/Login/LoginView.js
+++ Frontend/my-app/src/View/Login/LoginView.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./LoginView.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+
 export const LoginView = () => {
   return (
     <div className="login">
@@ -11,60 +14,56 @@ export const LoginView = () => {
           </h1>
           <div className="mb-8">
             <div class="flex justify-center items-center">
-              <label className="mr-">¿Eres nuevo usuario?</label>
-              <to
-              
-                className=" text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"
-              >
+              <label className="mr-2">¿Eres nuevo usuario?</label>
+              <a className=" cursor-pointer text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
                 Crear una cuenta
-              </to>
+              </a>
             </div>
           </div>
 
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
-            >
-              Username
-            </label>
+            <div className=" absolute ml-4 m-2">
+              <FontAwesomeIcon icon={  faUser} />
+            </div>
+
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
-              placeholder="Username"
+              placeholder=""
             />
           </div>
           <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              for="password"
-            >
-              Password
-            </label>
+         <div className="absolute ml-4 m-2">
+         <FontAwesomeIcon icon={faKey} />
+         </div>
             <input
               className="shadow appearance-none border b rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder="password"
+              placeholder=""
             />
           </div>
           <div className="flex items-center justify-between">
-					<div className="flex items-center">
-						<input id="recuerdame" name="recuerdame" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
-						<label for="remember_me" class="ml-2 block text-sm text-gray-900">
-							Recuerdame
-						</label>
-					</div>
+            <div className="flex items-center">
+              <input
+                id="recuerdame"
+                name="recuerdame"
+                type="checkbox"
+                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label for="remember_me" class="ml-2 block text-sm text-gray-900">
+                Recuerdame
+              </label>
+            </div>
 
-					<div className="text-sm">
-						<to className="font-medium text-indigo-600 hover:text-indigo-500">
-							Olvido su contraseña?
-						</to>
-					</div>
-				</div>
+            <div className="text-sm">
+              <a className=" cursor-pointer font-medium text-indigo-600 hover:text-indigo-500">
+                Olvido su contraseña?
+              </a>
+            </div>
+          </div>
           <div>
-          
             <button
               className="  block bg-teal-400 hover:bg-teal-600 font-bold text-black uppercase text-lg mx-auto p-2 rounded mt-4"
               type="button"
@@ -74,6 +73,11 @@ export const LoginView = () => {
           </div>
         </form>
       </div>
+      <img
+        className="ml-16 pt-32"
+        src="https://res.cloudinary.com/dytpump6i/image/upload/v1668705729/LifeBook2_2_1_kdgf1m.png"
+        alt=""
+      />
     </div>
   );
 };
