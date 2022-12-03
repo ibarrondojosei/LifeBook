5e025b46b018aae252353e61fd57afd0329b3d3a
Leandro Martini <leandromartini86@gmail.com>
2022-12-02 21:24:17 -0300

    merge2

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -3,15 +3,20 @@ import { LoginView } from './View/Login/LoginView';
 import {BrowserRouter as  Routes, Route} from 'react-router-dom';
 import ClinicalHistory from './View/ClinicalHistory/ClinicalHistory';
 import CicloLectivo from './View/CicloLectivo/CicloLectivo';
+import Footer, {footer} from './components/Footer/Footer.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
 
 function App() {
   return (
     <div className="App">
       {/* <CicloLectivo/> */}
-      {/* <LoginView/> */}
-      <ClinicalHistory/>
+      <LoginView/>
+      {/* <Footer/> */}
+      {/* <NavBar/> */}
+      {/* <ClinicalHistory/> */}
      
       {/* <RegisterView/> */}
+      
       
   
     </div>
