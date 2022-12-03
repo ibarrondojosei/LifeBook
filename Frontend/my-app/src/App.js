a7b972661c335192f83427fda85137ffabcb56be
Leandro Martini <leandromartini86@gmail.com>
2022-12-02 23:12:54 -0300

    routes

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,25 +1,27 @@
-
-import { LoginView } from './View/Login/LoginView';
-import {BrowserRouter as  Routes, Route} from 'react-router-dom';
-import ClinicalHistory from './View/ClinicalHistory/ClinicalHistory';
-import CicloLectivo from './View/CicloLectivo/CicloLectivo';
-import Footer, {footer} from './components/Footer/Footer.jsx';
-import NavBar from './components/NavBar/NavBar.jsx';
+import { LoginView } from "./View/Login/LoginView";
+import { RegisterView } from "./View/Register/RegisterView";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClinicalHistory from "./View/ClinicalHistory/ClinicalHistory";
+import CicloLectivo from "./View/CicloLectivo/CicloLectivo";
+import Footer, { footer } from "./components/Footer/Footer.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      {/* <CicloLectivo/> */}
-      <LoginView/>
-      {/* <Footer/> */}
-      {/* <NavBar/> */}
-      {/* <ClinicalHistory/> */}
-     
-      {/* <RegisterView/> */}
-      
-      
-  
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        {/* <NavBar /> */}
+
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="/clinicalHistory" element={<ClinicalHistory />} />
+        <Route path="/cicloLectivo" element={<CicloLectivo />} />
+
+        {/* <Footer/> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
